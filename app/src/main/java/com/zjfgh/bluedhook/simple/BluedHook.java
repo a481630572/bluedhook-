89c976b03ecf16eb1dca2a0cb5bca9b414e23046
a481630572 <70085892+a481630572@users.noreply.github.com>
2025-06-20 11:25:19 +0800

    Update BluedHook.java

--- app/src/main/java/com/zjfgh/bluedhook/simple/BluedHook.java
+++ app/src/main/java/com/zjfgh/bluedhook/simple/BluedHook.java
@@ -1,6 +1,5 @@
 package com.zjfgh.bluedhook.simple;
 
-
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
@@ -15,7 +14,6 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.java_websocket.WebSocket;
 import org.json.JSONObject;
 
 import java.util.Objects;
@@ -30,17 +28,10 @@ import de.robv.android.xposed.callbacks.XC_LayoutInflated;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class BluedHook implements IXposedHookLoadPackage, IXposedHookInitPackageResources, IXposedHookZygoteInit {
-    public static WSServerManager wsServerManager;
-
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam param) {
         if (param.packageName.equals("com.soft.blued")) {
             XposedHelpers.findAndHookMethod("com.soft.blued.StubWrapperProxyApplication", param.classLoader, "initProxyApplication", Context.class, new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
-                    super.beforeHookedMethod(param);
-                }
-
                 @Override
                 protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                     super.afterHookedMethod(param);
@@ -48,76 +39,9 @@ public class BluedHook implements IXposedHookLoadPackage, IXposedHookInitPackage
                     AppContainer.getInstance().setBluedContext(bluedContext);
                     AppContainer.getInstance().setClassLoader(bluedContext.getClassLoader());
                     Toast.makeText(bluedContext, "外挂成功！", Toast.LENGTH_LONG).show();
-                    try {
-                        VoiceTTS.getInstance(bluedContext);
-                    } catch (Exception e) {
-                        Log.e("BluedHook", "语音合成模块异常：\n" +
-                                e);
-                    }
-                    NetworkManager.getInstance();
+                    
+                    // 仅保留UserInfoFragmentNewHook
                     UserInfoFragmentNewHook.getInstance(bluedContext, AppContainer.getInstance().getModuleRes());
-                    LiveHook.getInstance(bluedContext);
-                    PlayingOnLiveBaseModeFragmentHook.getInstance(bluedContext, AppContainer.getInstance().getModuleRes());
-                    FragmentMineNewBindingHook.getInstance(bluedContext, AppContainer.getInstance().getModuleRes());
-                    LiveMultiBoyItemViewHook.getInstance();
-                    ChatHook.getInstance(bluedContext, AppContainer.getInstance().getModuleRes());
-                    NearbyPeopleFragment_ViewBindingHook.getInstance(AppContainer.getInstance().getBluedContext(), AppContainer.getInstance().getClassLoader());
-                    HornViewNewHook.autoHornViewNew();
-                    LikeFollowModel.getInstance(bluedContext, AppContainer.getInstance().getModuleRes());
-                    wsServerManager = new WSServerManager(new WSServerManager.WSServerListener() {
-                        @Override
-                        public void onServerStarted(int port) {
-                            ModuleTools.showBluedToast("WS服务已启动在" + port + "端口上");
-                        }
-
-                        @Override
-                        public void onServerStopped() {
-                            ModuleTools.showBluedToast("WS服务已停止");
-                        }
-
-                        @Override
-                        public void onServerError(String error) {
-                            ModuleTools.showBluedToast("WS服务发生了错误：" + error);
-                        }
-
-                        @Override
-                        public void onClientConnected(String address) {
-
-                        }
-
-                        @Override
-                        public void onClientDisconnected(String address) {
-
-                        }
-
-                        @Override
-                        public void onMessageReceived(WebSocket conn, String message) {
-                            if (message.equals("同步数据")) {
-                                try {
-                                    // 1. 构建基础响应结构
-                                    JSONObject response = new JSONObject();
-                                    response.put("msgType", 1995);
-
-                                    // 2. 构建msgExtra部分
-                                    JSONObject msgExtra = new JSONObject();
-                                    msgExtra.put("msgType", "lotteryRecords");
-
-                                    // 3. 获取并转换文件数据为JSON
-                                    JSONObject recordsData = new FileToJsonConverter().convertFilesToJson();
-                                    msgExtra.put("msgExtra", recordsData);
-
-                                    // 4. 将msgExtra放入主响应
-                                    response.put("msgExtra", msgExtra);
-                                    // 5. 广播消息
-                                    String jsonResponse = response.toString();
-                                    Log.d("WebSocketServer", "Broadcasting records: " + jsonResponse);
-                                    wsServerManager.broadcastMessage(jsonResponse);
-                                } catch (Exception e) {
-                                    Log.e("WebSocketServer", "Error processing sync request", e);
-                                }
-                            }
-                        }
-                    });
                 }
             });
         }
@@ -138,10 +62,13 @@ public class BluedHook implements IXposedHookLoadPackage, IXposedHookInitPackage
                     int scrollView1ID = bluedContext.getResources().getIdentifier("scrollView1", "id", bluedContext.getPackageName());
                     ScrollView scrollView = liParam.view.findViewById(scrollView1ID);
                     LinearLayout scrollLinearLayout = (LinearLayout) scrollView.getChildAt(0);
+                    
+                    // 保留设置界面布局
                     LinearLayout mySettingsLayoutAu = (LinearLayout) inflater.inflate(moduleRes.getLayout(R.layout.module_settings_layout), null, false);
                     TextView auCopyTitleTv = mySettingsLayoutAu.findViewById(R.id.settings_name);
                     auCopyTitleTv.setText("复制授权信息(请勿随意泄漏)");
                     mySettingsLayoutAu.setOnClickListener(v -> AuthManager.auHook(true, AppContainer.getInstance().getClassLoader(), bluedContext));
+                    
                     LinearLayout moduleSettingsLayout = (LinearLayout) inflater.inflate(moduleRes.getLayout(R.layout.module_settings_layout), null, false);
                     TextView moduleSettingsTitleTv = moduleSettingsLayout.findViewById(R.id.settings_name);
                     moduleSettingsTitleTv.setText("外挂模块设置");
@@ -151,22 +78,21 @@ public class BluedHook implements IXposedHookLoadPackage, IXposedHookInitPackage
                         dialog.getWindow().setLayout(100, 300);
                         dialog.setOnShowListener(dialogInterface -> {
                             View parentView = dialog.getWindow().getDecorView();
-                            parentView.setBackgroundColor(Color.parseColor("#F7F6F7")); // 自定义背景色
+                            parentView.setBackgroundColor(Color.parseColor("#F7F6F7"));
                         });
                         dialog.show();
-
                     });
+                    
                     scrollLinearLayout.addView(mySettingsLayoutAu, 0);
                     scrollLinearLayout.addView(moduleSettingsLayout, 1);
                 }
 
                 private AlertDialog getAlertDialog(LayoutInflatedParam liParam) {
+                    // 移除WebSocket相关逻辑，保留设置界面创建
                     SettingsViewCreator creator = new SettingsViewCreator(liParam.view.getContext());
                     View settingsView = creator.createSettingsView();
                     creator.setOnSwitchCheckedChangeListener((functionId, isChecked) -> {
-                        if (functionId == SettingsViewCreator.ANCHOR_MONITOR_LIVE_HOOK) {
-                            LiveHook.getInstance(AppContainer.getInstance().getBluedContext()).setAnchorMonitorIvVisibility(isChecked);
-                        }
+                        // 空实现，避免依赖已删除的功能
                     });
                     AlertDialog.Builder builder = new AlertDialog.Builder(liParam.view.getContext());
                     builder.setView(settingsView);
@@ -175,7 +101,6 @@ public class BluedHook implements IXposedHookLoadPackage, IXposedHookInitPackage
             });
         }
     }
-
 
     @Override
     public void initZygote(StartupParam startupParam) {
