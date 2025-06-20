e3e8da2b2be6eee64629c26e744d5f2c58326260
a481630572 <70085892+a481630572@users.noreply.github.com>
2025-06-20 12:00:14 +0800

    Update BluedHook.java

--- app/src/main/java/com/zjfgh/bluedhook/simple/BluedHook.java
+++ app/src/main/java/com/zjfgh/bluedhook/simple/BluedHook.java
@@ -23,6 +23,7 @@ import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_InitPackageResources;
 import de.robv.android.xposed.callbacks.XC_LayoutInflated;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class BluedHook implements IXposedHookLoadPackage, IXposedHookInitPackageResources, IXposedHookZygoteInit {
     @Override
